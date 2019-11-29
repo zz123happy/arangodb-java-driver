5864dbac52baf84fddc627c595721b360d36c476
Michele Rastelli <rashtao@gmail.com>
2019-11-29 14:47:27 +0100

    ContainerDeployment

--- src/test/java/containers/ClusterContainer.java
+++ src/test/java/containers/ClusterContainer.java
@@ -1,7 +1,7 @@
 package containers;
 
 
-import org.junit.jupiter.api.Test;
+import com.arangodb.next.connection.HostDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.Container;
@@ -17,77 +17,44 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class ClusterContainer {
+public class ClusterContainer implements ContainerDeployment {
 
     private final Logger log = LoggerFactory.getLogger(ClusterContainer.class);
-    private final String DOCKER_IMAGE = ContainerUtils.getImage();
     private final String DOCKER_COMMAND = "arangodb --auth.jwt-secret /jwtSecret ";
 
-    private final AtomicInteger createdAgents = new AtomicInteger();
-    private final AtomicInteger createdDbServers = new AtomicInteger();
-    private final AtomicInteger createdCoordinators = new AtomicInteger();
+    private final AtomicInteger createdAgentsCount = new AtomicInteger();
+    private final AtomicInteger createdDbServersCount = new AtomicInteger();
+    private final AtomicInteger createdCoordinatorsCount = new AtomicInteger();
 
-    private final Network network = Network.newNetwork();
+    private final Network network;
 
     private final List<GenericContainer> agents;
     private final List<GenericContainer> dbServers;
     private final List<GenericContainer> coordinators;
 
-    public ClusterContainer() {
+    ClusterContainer(int dbServers, int coordinators) {
+        network = Network.newNetwork();
+
         agents = Arrays.asList(
                 createAgent(),
                 createAgent(),
                 createAgent()
         );
-        dbServers = Arrays.asList(
-                createDbServer(),
-                createDbServer()
-        );
-        coordinators = Arrays.asList(
-                createCoordinator(),
-                createCoordinator()
-        );
+
+        this.dbServers = IntStream.range(0, dbServers)
+                .mapToObj(i -> createDbServer())
+                .collect(Collectors.toList());
+
+        this.coordinators = IntStream.range(0, coordinators)
+                .mapToObj(i -> createCoordinator())
+                .collect(Collectors.toList());
     }
 
-    private GenericContainer createContainer(String name, int port) {
-        return new GenericContainer(DOCKER_IMAGE)
-                .withCopyFileToContainer(MountableFile.forClasspathResource("containers/jwtSecret"), "/jwtSecret")
-                .withExposedPorts(port)
-                .withNetwork(network)
-                .withNetworkAliases(name)
-                .withLogConsumer(new Slf4jLogConsumer(log).withPrefix("[" + name + "]"))
-                .waitingFor(Wait.forLogMessage(".*up and running.*", 1).withStartupTimeout(Duration.ofSeconds(300)));
-    }
-
-    private GenericContainer createAgent() {
-        int count = createdAgents.incrementAndGet();
-        String joinParameter = count == 1 ? " " : "--starter.join agent1 ";
-        return createContainer("agent" + count, 8531)
-                .withCommand(DOCKER_COMMAND + "--cluster.start-dbserver false --cluster.start-coordinator false " + joinParameter);
-    }
-
-    private GenericContainer createDbServer() {
-        int count = createdDbServers.incrementAndGet();
-        return createContainer("dbserver" + count, 8530)
-                .withCommand(DOCKER_COMMAND + "--cluster.start-dbserver true --cluster.start-coordinator false --starter.join agent1");
-    }
-
-    private GenericContainer createCoordinator() {
-        int count = createdCoordinators.incrementAndGet();
-        return createContainer("coordinator" + count, 8529)
-                .withCommand(DOCKER_COMMAND + "--cluster.start-dbserver false --cluster.start-coordinator true --starter.join agent1");
-    }
-
-    private CompletableFuture<Void> performActionOnGroup(List<GenericContainer> group, Consumer<GenericContainer> action) {
-        return CompletableFuture.allOf(
-                group.stream()
-                        .map(it -> CompletableFuture.runAsync(() -> action.accept(it)))
-                        .toArray(CompletableFuture[]::new)
-        );
-    }
-
-    public CompletableFuture<ClusterContainer> start() {
+    @Override
+    public CompletableFuture<ContainerDeployment> start() {
         return CompletableFuture.completedFuture(null)
                 .thenCompose(v -> performActionOnGroup(agents, GenericContainer::start))
                 .thenCompose(v -> performActionOnGroup(dbServers, GenericContainer::start))
@@ -120,7 +87,8 @@ public class ClusterContainer {
                 .thenApply((v) -> this);
     }
 
-    public CompletableFuture<ClusterContainer> close() {
+    @Override
+    public CompletableFuture<ContainerDeployment> stop() {
         return CompletableFuture.allOf(
                 performActionOnGroup(agents, GenericContainer::stop),
                 performActionOnGroup(dbServers, GenericContainer::stop),
@@ -130,15 +98,48 @@ public class ClusterContainer {
                 .thenApply((v) -> this);
     }
 
-    public List<GenericContainer> getCoordinators() {
-        return coordinators;
+    @Override
+    public List<HostDescription> getHosts() {
+        return coordinators.stream()
+                .map(it -> HostDescription.of(it.getContainerIpAddress(), it.getFirstMappedPort()))
+                .collect(Collectors.toList());
     }
 
-    @Test
-    void startCluster() {
-        ClusterContainer cluster = new ClusterContainer().start().join();
-        cluster.getCoordinators().forEach(it -> System.out.println(it.getContainerIpAddress() + ":" + it.getFirstMappedPort()));
-        cluster.close().join();
+    private GenericContainer createContainer(String name, int port) {
+        return new GenericContainer(getImage())
+                .withCopyFileToContainer(MountableFile.forClasspathResource("containers/jwtSecret"), "/jwtSecret")
+                .withExposedPorts(port)
+                .withNetwork(network)
+                .withNetworkAliases(name)
+                .withLogConsumer(new Slf4jLogConsumer(log).withPrefix("[" + name + "]"))
+                .waitingFor(Wait.forLogMessage(".*up and running.*", 1).withStartupTimeout(Duration.ofSeconds(300)));
+    }
+
+    private GenericContainer createAgent() {
+        int count = createdAgentsCount.incrementAndGet();
+        String joinParameter = count == 1 ? " " : "--starter.join agent1 ";
+        return createContainer("agent" + count, 8531)
+                .withCommand(DOCKER_COMMAND + "--cluster.start-dbserver false --cluster.start-coordinator false " + joinParameter);
+    }
+
+    private GenericContainer createDbServer() {
+        int count = createdDbServersCount.incrementAndGet();
+        return createContainer("dbserver" + count, 8530)
+                .withCommand(DOCKER_COMMAND + "--cluster.start-dbserver true --cluster.start-coordinator false --starter.join agent1");
+    }
+
+    private GenericContainer createCoordinator() {
+        int count = createdCoordinatorsCount.incrementAndGet();
+        return createContainer("coordinator" + count, 8529)
+                .withCommand(DOCKER_COMMAND + "--cluster.start-dbserver false --cluster.start-coordinator true --starter.join agent1");
+    }
+
+    private CompletableFuture<Void> performActionOnGroup(List<GenericContainer> group, Consumer<GenericContainer> action) {
+        return CompletableFuture.allOf(
+                group.stream()
+                        .map(it -> CompletableFuture.runAsync(() -> action.accept(it)))
+                        .toArray(CompletableFuture[]::new)
+        );
     }
 
 }
