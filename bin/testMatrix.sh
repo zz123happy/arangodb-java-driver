8bfac45c5984bb8fdf77b1f5d60a0f3e11ce3e67
Michele Rastelli <rashtao@gmail.com>
2019-12-09 12:20:52 +0100

    scripts comments

--- bin/testMatrix.sh
+++ bin/testMatrix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# execute the tests for every docker image in the matrix
+# every failing test will generate a logfile named like out-$(date +%s%N).txt
+
 for img in \
   docker.io/arangodb/arangodb:3.4.8 \
   docker.io/arangodb/enterprise:3.4.8 \
--- bin/testSingle.sh
+++ bin/testSingle.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# runs the tests against the docker image provided as argument
+# logs are redirected to a generate logfile named like out-$(date +%s%N).txt
+# if tests are successfull the logfile will be removed
+
+# USAGE:
+#   export ARANGO_LICENSE_KEY=<arangodb-enterprise-license>
+#   ./testSingle.sh <dockerImage>
+
+# EXAMPLE:
+#   ./testSingle.sh docker.io/arangodb/arangodb:3.5.3
+
+
 # exit when any command fails
 set -e
 
@@ -10,4 +22,6 @@ echo "==================================================="
 docker pull "$1"
 logfile=out-$(date +%s%N).txt
 mvn clean test -e -Dtest.docker.image="$1" -Darango.license.key="$ARANGO_LICENSE_KEY" >"$logfile" 2>&1
+
+# remove logfile if the test completed successfully
 rm "$logfile"
