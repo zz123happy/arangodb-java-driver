71ce09cd2147265c329c1cdfba10af26eb6ad7a3
Michele Rastelli <rashtao@gmail.com>
2019-12-20 14:00:00 +0100

    start db with TLSv1.3

--- docker/start_db_single_ssl.sh
+++ docker/start_db_single_ssl.sh
@@ -5,11 +5,11 @@
 #   ./start_db_single_ssl.sh <dockerImage>
 
 # EXAMPLE:
-#   ./start_db_single_ssl.sh docker.io/arangodb:3.5.0
+#   ./start_db_single_ssl.sh docker.io/arangodb:3.5.3
 
 docker pull "$1"
 
-docker run -d -p 8529:8529 -e ARANGO_ROOT_PASSWORD=test -e ARANGO_LICENSE_KEY="$ARANGO_LICENSE_KEY" -v $(pwd)/server.pem:/server.pem "$1" arangod --ssl.keyfile /server.pem --server.endpoint ssl://0.0.0.0:8529
+docker run -d -p 8529:8529 -e ARANGO_ROOT_PASSWORD=test -e ARANGO_LICENSE_KEY="$ARANGO_LICENSE_KEY" -v $(pwd)/server.pem:/server.pem "$1" arangod --ssl.keyfile /server.pem --server.endpoint ssl://0.0.0.0:8529 --ssl.protocol 6
 
 echo "waiting for arangodb ..."
 
