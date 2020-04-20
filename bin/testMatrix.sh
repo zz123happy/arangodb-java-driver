dbe88cb2336170aacc86e84c20445ae8fb08b8c9
Michele Rastelli <rashtao@gmail.com>
2020-04-20 21:33:09 +0200

    test matrix upd

--- bin/testMatrix.sh
+++ bin/testMatrix.sh
@@ -4,12 +4,12 @@
 # every failing test will generate a logfile named like out-$(date +%s%N).txt
 
 for img in \
-  docker.io/arangodb/arangodb:3.4.9 \
-  docker.io/arangodb/enterprise:3.4.9 \
   docker.io/arangodb/arangodb:3.5.4 \
   docker.io/arangodb/enterprise:3.5.4 \
   docker.io/arangodb/arangodb:3.6.2 \
-  docker.io/arangodb/enterprise:3.6.2; do
+  docker.io/arangodb/enterprise:3.6.2 \
+  docker.io/arangodb/arangodb-preview:3.7.0-alpha.2 \
+  docker.io/arangodb/enterprise-preview:3.7.0-alpha.2; do
   ./bin/testSingle.sh $img
 done
 
